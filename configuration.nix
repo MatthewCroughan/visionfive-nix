01b3abaf226bfcb97508a5c799bad62cdcb1bc57
matthewcroughan <matt@croughan.sh>
2022-07-13 07:41:39 +0100

    configuration: enable avahi, set hostname, add comments

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,14 @@
 {
+  networking.hostName = "visionfive-nix";
+
   # Enable ssh on boot
   services.openssh.enable = true;
+
+  # Open port 19999 for Netdata
   networking.firewall.allowedTCPPorts = [ 19999 ];
   services.netdata.enable = true;
+
+  # Enable Avahi mDNS, you should be able to reach http://visionfive-nix:19999
+  # to reach netdata when booted
+  services.avahi.enable = true;
 }
