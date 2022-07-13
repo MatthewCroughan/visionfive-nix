d04dbc0febdca4646425d58a62443e06bf6ef6f8
matthewcroughan <matt@croughan.sh>
2022-07-13 07:43:23 +0100

    configuration: add commented out wireless example

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,16 @@
 {
+#  networking = {
+#    interfaces."wlan0".useDHCP = true;
+#    wireless = {
+#      interfaces = [ "wlan0" ];
+#      enable = true;
+#      networks = {
+#        myWifiNetworkSSID.pskRaw = "29f4be0s82e33c18149cdcfc869f84ce6a8831fb492e35d759468f6103bf8a31"; # pskRaw is the result of running wpa_passphrase 'SSID' 'PASSWORD'
+#        WIFI_SSID.psk = "WIFI_PASSWORD";
+#      };
+#    };
+#  };
+
   networking.hostName = "visionfive-nix";
 
   # Enable ssh on boot
